29975b681eb28b7599d6ced3100c1448581fba34
czovek.dominik.noel <czovekdominik@gmail.com>
2025-09-30 14:40:42 +0200

    uj repo mert mindig rossz volt a crud es bajo volt a repokban levok mappakkal de tanar ur is tudja hiszen segitett ezert van most hirtelen feltoltve az egesz

--- Vizallasuj2/Pages/Index.cshtml.cs
+++ Vizallasuj2/Pages/Index.cshtml.cs
@@ -1,29 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Vizallasuj2.Data;
 using Vizallasuj2.Models;
 
-namespace Vizallasuj2.Pages
+namespace vizallas.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly Vizallasuj2.Data.vizDbContext _context;
+        private readonly vizDbContext _context;
+        public IndexModel(vizDbContext context) => _context = context;
 
-        public IndexModel(Vizallasuj2.Data.vizDbContext context)
-        {
-            _context = context;
-        }
+        [BindProperty(SupportsGet = true)]
+        public string? FolyoParam { get; set; }
 
-        public IList<Víz> Víz { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? VarosParam { get; set; }
+
+        public List<string> Folyo { get; set; } = new();
+
+        public List<string> Varos { get; set; } = new();
+        [BindProperty(SupportsGet = true)]
+        public IList<Vizállás> VizallasLista { get; set; } = new List<Vizállás>();
 
         public async Task OnGetAsync()
         {
-            Víz = await _context.Vízállás.ToListAsync();
+            Folyo = await _context.Vízállás
+                .Select(x => x.Folyo)
+                .Distinct()
+                .OrderBy(x => x)
+                .ToListAsync();
+
+            Varos = await _context.Vízállás
+                .Select(x => x.Varos)
+                .Distinct()
+                .OrderBy(x => x)
+                .ToListAsync();
+
+            var q = _context.Vízállás.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(FolyoParam))
+                q = q.Where(x => x.Folyo == FolyoParam);
+
+            if (!string.IsNullOrWhiteSpace(VarosParam))
+                q = q.Where(x => x.Varos == VarosParam);
+
+            VizallasLista = await q
+                .OrderBy(x => x.Varos)
+                .ThenBy(x => x.Datum)
+                .ToListAsync();
         }
     }
 }
---
+++ Vizallasuj2/Pages/Summary.cshtml.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Vizallasuj2.Data;
+
+namespace Vizallasuj2.Pages
+{
+    public class SummaryRow
+    {
+        public string Varos { get; set; } = string.Empty;
+        public int MinValue { get; set; }
+        public DateTime MinDate { get; set; }
+        public int MaxValue { get; set; }
+        public DateTime MaxDate { get; set; }
+    }
+
+    public class SummaryModel : PageModel
+    {
+        private readonly vizDbContext _context;
+        public SummaryModel(vizDbContext context) => _context = context;
+
+        [BindProperty(SupportsGet = true)]
+        public string? Folyo { get; set; }
+
+        public List<string> Folyok { get; set; } = new();
+        public List<SummaryRow> Rows { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            Folyok = await _context.Vizállás
+                .Select(x => x.Folyo)
+                .Distinct()
+                .OrderBy(x => x)
+                .ToListAsync();
+
+            Folyo ??= Folyok.FirstOrDefault();
+            if (string.IsNullOrWhiteSpace(Folyo)) return;
+
+            var data = await _context.Vizállás
+                .Where(x => x.Folyo == Folyo)
+                .ToListAsync();
+
+            Rows = data
+                .GroupBy(x => x.Varos)
+                .Select(g =>
+                {
+                    var min = g.OrderBy(x => x.Ertek).First();
+                    var max = g.OrderByDescending(x => x.Ertek).First();
+
+                    return new SummaryRow
+                    {
+                        Varos = g.Key,
+                        MinValue = min.Ertek,
+                        MinDate = min.Datum,
+                        MaxValue = max.Ertek,
+                        MaxDate = max.Datum
+                    };
+                })
+                .OrderBy(x => x.Varos)
+                .ToList();
+        }
+    }
+}
